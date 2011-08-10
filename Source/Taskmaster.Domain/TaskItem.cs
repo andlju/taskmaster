030b404f5b97905cb097ad4c4df050e39e66d981
Anders Ljusberg <andlju@gmail.com>
2011-08-10 21:52:46 +0200

    A few first step towards anemia

---
+++ Source/Taskmaster.Domain/TaskItem.cs
@@ -0,0 +1,30 @@
+﻿using Petite;
+
+namespace Taskmaster.Domain
+{
+    public class TaskItem
+    {
+        public int TaskItemId { get; set; }
+        public string Title { get; set; }
+        public string Details { get; set; }
+
+        public User AssignedTo { get; set; }
+        public User CreatedBy { get; set; }
+    }
+
+    public class User
+    {
+        public int UserId { get; set; }
+        public string Name { get; set; }
+    }
+
+    public interface ITaskItemRepository : IRepository<TaskItem>
+    {
+        
+    }
+
+    public interface IUserRepository : IRepository<User>
+    {
+        
+    }
+}
---
+++ Source/Taskmaster.Service/ITaskService.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Taskmaster.Service
+{
+    public class User
+    {
+        public int MemberId { get; set; }
+
+        public string Name { get; set; }
+
+    }
+
+    public class TaskItem
+    {
+        public int TaskItemId { get; set; }
+        public string Title { get; set; }
+        public string Details { get; set; }
+
+        public DateTime DueDate { get; set; }
+
+        public int AssignedMemberId { get; set; }
+    }
+
+    public class RequestBase
+    {
+        public int UserId { get; set; }
+    }
+
+    public class ResponseBase
+    {
+        public int StatusCode { get; set; }
+    }
+
+    public class AddTaskItemRequest : RequestBase
+    {
+        public string Title { get; set; }
+        public string Details { get; set; }
+    }
+
+    public class AddTaskItemResponse : ResponseBase
+    {
+        public TaskItem TaskItem { get; set; }
+    }
+
+    public class EditTaskItemRequest : RequestBase
+    {
+        public int TaskItemId { get; set; }
+        public string Title { get; set; }
+        public string Details { get; set; }
+    }
+
+    public class EditTaskItemResponse : ResponseBase
+    {
+        public TaskItem TaskItem { get; set; }
+    }
+
+    public class FindTaskItemByNameRequest : RequestBase
+    {
+        public string Query { get; set; }
+    }
+
+    public class FindTaskItemByNameResponse : ResponseBase
+    {
+        public List<TaskItem> TaskItems { get; set; }
+    }
+
+    public class FindTaskItemByAssignedUserRequest : RequestBase
+    {
+        public string AssignedUserId { get; set; }
+    }
+
+    public class FindTaskItemByAssignedUserResponse : ResponseBase
+    {
+        public List<TaskItem> TaskItems { get; set; }
+    }
+
+    public interface ITaskService
+    {
+        AddTaskItemResponse AddTaskItem(AddTaskItemRequest request);
+        EditTaskItemResponse EditTaskItem(EditTaskItemRequest request);
+
+        FindTaskItemByNameResponse FindTaskItemByName(FindTaskItemByNameRequest request);
+        FindTaskItemByAssignedUserResponse FindTaskItemByAssignedUser(FindTaskItemByAssignedUserRequest request);
+    }
+}
